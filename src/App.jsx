5547e266531a5db00c97915b750b9d1f7772f712
AndresSanchez14 <asanchesvera@yahoo.es>
2023-05-03 08:45:20 -0500

    Muchos cambios integracion parte de lina y cambios en el fotter

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Posts from "./components/Posts";
 import UserContext from "./context/User";
 import Admin from "./components/Admin";
 import Footer from "./components/Footer";
-
+import Form from "./components/Form";
 const marioImagenes = [
   "../public/Mpeach2.jpeg",
   "../public/Peach.jpeg",
@@ -20,6 +20,7 @@ const App = function () {
       <Navbar />
       <Slider imagenes={marioImagenes} />
       {!user || user.roll != "admin" ? <Posts /> : <Admin />}
+      <Form />
       <Footer />
     </>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,27 +4,7 @@ import FooterStyle from "../styles/Footer.module.css";
 function Footer() {
   return (
     <>
-      <section className={FooterStyle.video}>
-        <h1>TRAILER PELICULA 2023</h1>
-        <iframe
-          width="1280"
-          height="600"
-          src="https://www.youtube.com/embed/8YQM6VlfiqM"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-
-        <article className={FooterStyle.infovideo}>
-          <a href="https://www.youtube.com/watch?v=Yo-fQiQrOtA&t=33s">
-            ver pelicula
-          </a>
-          <p>
-            Mira el video y no te pierdas de lo nuevo de mario bross y nintendo
-          </p>
-        </article>
-
+      <section className={FooterStyle.container}>
         <footer className={FooterStyle.footer}>
           <a href=""> Servicio al cliente </a>
           <i class="fa-brands fa-facebook"></i>
