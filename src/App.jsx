b1833d55babaaa84770f0b632a5bfaf3516e8f29
linasanchez38 <125481093+linasanchez38@users.noreply.github.com>
2023-05-04 10:42:23 +0000

    App.jsx completa/terminada, CSS terminado, mediaquery y cambios finales. PRESENTACIÓN!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React, { useContext } from "react";
 import Navbar from "./components/Navbar";
 import Slider from "./components/Slider";
-import Posts from "./components/Posts";
 import UserContext from "./context/User";
-import Admin from "./components/Admin";
-import Footer from "./components/Footer";
-import cssluigi from "./styles/Luigis.css";
-import textos from "./styles/Textos.css";
-import Historia from "./styles/Historia.css";
-import Form from "./components/Form";
-import Mario from "./personajes/Mario";
+import Personas from "./styles/Personas.css";
+import History from "./styles/History.css";
 import Luigi from "./personajes/Luigi";
+import Mario from "./personajes/Mario";
+import Posts from "./components/Posts";
+import Admin from "./components/Admin";
+import Form from "./components/Form";
+import Footer from "./components/Footer";
 
 const marioImagenes = [
   "../public/Mpeach2.jpeg",
@@ -27,7 +26,6 @@ const App = function () {
       <Slider imagenes={marioImagenes} />
       <Luigi />
       <Mario />
-
       {!user || user.roll != "admin" ? <Posts /> : <Admin />}
       <Form />
       <Footer />
